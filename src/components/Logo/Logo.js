07a26802c8fed3df0d68591939673734d3da1aac
Kiran Joshi <Kiran.Joshi@Agilisys.co.uk>
2018-12-08 14:21:40 +0530

    GOODREADS BOOKS : App bar logo updated & minor improvments in appbar css

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import bookLogo from '../../assets/images/logo.jpg';
+import classes from './Logo.css';
+
+const logo = () => (
+        <img src={bookLogo} alt="Good Read Books" />
+);
+
+export default logo;
