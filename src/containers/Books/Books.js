2105e9d32c33795900fe4302768c055f5479c6ba
Kiran Joshi <Kiran.Joshi@Agilisys.co.uk>
2018-12-09 13:58:09 +0530

    GOODREADS BOOKS : integrated with  backend API

--- src/containers/Books/Books.js
+++ src/containers/Books/Books.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-// import Aux from '../../hoc/_Aux/_Aux';
-// import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-books';
 import Grid from '@material-ui/core/Grid'
 import PropTypes from 'prop-types';
@@ -17,33 +16,53 @@ const styles = theme => ({
 class Books extends React.Component {
 
     state = {
-        books: []
+        books: [],
+        prevSearch: "",
+        errorMessage: "",
+        loading : true
     }
 
     constructor(props) {
         super(props);
+        this.state.prevSearch = this.props.searchString
         this.getBooks()
     }
 
     getBooks() {
-        console.log(this.props.searchString);
-        axios.get('data/bookslist.json')
+        
+        axios.post('/api/books/search', {
+            search: this.props.searchString
+        })
             .then(response => {
-                // console.log(response.data.data.GoodreadsResponse.search.results.work)
-                this.setState({ books: response.data.data.GoodreadsResponse.search.results.work });
+                this.setState({ loading: false });
+                if (response.data.data.GoodreadsResponse.search['total-results'] > 0) {
+                    this.setState({ books: response.data.data.GoodreadsResponse.search.results.work });
+                } else {
+                    this.setState({ books: [] });
+                    this.setState({ errorMessage: 'No books found for selected search!' });
+                }
+
             })
             .catch(error => {
-                // this.setState( { error: true } );
+                this.setState({ loading: false });
+                this.setState({ errorMessage: 'Something went wrong please try agian later' });
             });
     }
 
     render() {
+        if (this.props.searchString.toLowerCase() !== this.state.prevSearch.toLowerCase()) {
+            this.setState({ loading: true });
+            this.setState({ books: [] });
+            this.setState({ prevSearch: this.props.searchString });
+            this.getBooks();
+        }
+
         return (
             <div style={{ overflow: 'hidden' }}>
                 <Divider className="mt20 mb20" />
                 <span className="pd10"><b>Search result books for : </b> {this.props.searchString}</span>
                 <Divider className="mt20" />
-                {this.state.books ? (
+                {this.state.books.length > 0 ? (
                     <Grid container spacing={24} style={{ padding: 24 }}>
                         {this.state.books.map(currentBook => (
                             <Grid key={currentBook.id} item xs={6} md={3} sm={4} lg={3} xl={2}>
@@ -51,7 +70,14 @@ class Books extends React.Component {
                             </Grid>
                         ))}
                     </Grid>
-                ) : "No books found"}
+                ) :
+                    <div className="mt20">
+                        <div className="errorMessage"><b>{this.state.errorMessage}</b></div>
+                    </div>
+                }
+
+               {this.state.loading ? (<Spinner />) :''}
+               <Divider className="mt20 mb20" />
             </div>
         )
     }
@@ -60,7 +86,7 @@ class Books extends React.Component {
 
 Books.propTypes = {
     classes: PropTypes.object.isRequired,
-    callback : PropTypes.func
+    callback: PropTypes.func
 };
 
 export default withStyles(styles)(Books);
